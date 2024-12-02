45594e8097bc3428e3b50f973bcd3bfb95b706b9
ikyrie <mah.c5r@gmail.com>
2024-12-02 17:23:35 -0300

    Adicionando GoogleFonts

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     seedColor: Colors.blueGrey,
     brightness: Brightness.dark,
-  )
+  ),
+  textTheme: TextTheme(
+    bodyMedium: GoogleFonts.montserrat(),
+  ),
 );
