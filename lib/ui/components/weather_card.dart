6976ef8b545c5ea2990b4096c4ce37057c463e91
ikyrie <mah.c5r@gmail.com>
2024-12-03 16:21:50 -0300

    Criando o componente vertical de clima

--- lib/ui/components/weather_card.dart
+++ lib/ui/components/weather_card.dart
@@ -37,6 +37,44 @@ class WeatherCard extends StatelessWidget {
   }
 }
 
+
+class VerticalWeatherCard extends StatelessWidget {
+  const VerticalWeatherCard({super.key, required this.weekDay, required this.temperature, required this.date});
+
+  final String weekDay;
+  final String date;
+  final String temperature;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: const BoxDecoration(
+          color: fillTransparente,
+          borderRadius: BorderRadius.all(
+            Radius.circular(15),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(weekDay),
+            const SizedBox(height: 4,),
+            Text(date),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: Icon(Icons.sunny),
+            ),
+            Text(temperature, style: largeSemibold,),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class DetailedWeatherCard extends StatelessWidget {
   const DetailedWeatherCard({super.key});
 
