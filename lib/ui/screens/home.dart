29c6010853fcec5afd408fc43692ad586dc8a896
ikyrie <mah.c5r@gmail.com>
2024-12-03 16:39:06 -0300

    Arrumando o "bug" do teclado virtual quebrando a altura do container

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -13,6 +13,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Ink(
+        height: double.infinity,
         padding: const EdgeInsets.fromLTRB(32, 80, 32, 0),
         decoration: const BoxDecoration(
           gradient: LinearGradient(
@@ -20,16 +21,18 @@ class Home extends StatelessWidget {
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter),
         ),
-        child: Column(
-          children: <Widget>[
-            CustomSearchField(controller: cityTextController),
-            const SizedBox(height: 32,),
-            WeatherCard(cityName: "Florianópolis - SC", temperature: "25°", onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context) => WeatherDetails(cityName: "Florianópolis - SC")));},),
-            WeatherCard(cityName: "Curitiba - PR", temperature: "21°", onTap: (){},),
-            WeatherCard(cityName: "São Paulo - SP", temperature: "32°", onTap: (){},),
-            WeatherCard(cityName: "Porto Alegre - RS", temperature: "19°", onTap: (){},),
-            WeatherCard(cityName: "Toledo - PR", temperature: "26°", onTap: (){},),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              CustomSearchField(controller: cityTextController),
+              const SizedBox(height: 32,),
+              WeatherCard(cityName: "Florianópolis - SC", temperature: "25°", onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context) => WeatherDetails(cityName: "Florianópolis - SC")));},),
+              WeatherCard(cityName: "Curitiba - PR", temperature: "21°", onTap: (){},),
+              WeatherCard(cityName: "São Paulo - SP", temperature: "32°", onTap: (){},),
+              WeatherCard(cityName: "Porto Alegre - RS", temperature: "19°", onTap: (){},),
+              WeatherCard(cityName: "Toledo - PR", temperature: "26°", onTap: (){},),
+            ],
+          ),
         ),
       ),
     );
