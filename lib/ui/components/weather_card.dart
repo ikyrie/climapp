76138e735b7d671dbe5fd285e51354fa28261b33
ikyrie <mah.c5r@gmail.com>
2024-12-03 15:57:58 -0300

    Criando o cartão de detalhes de clima

--- lib/ui/components/weather_card.dart
+++ lib/ui/components/weather_card.dart
@@ -36,3 +36,68 @@ class WeatherCard extends StatelessWidget {
     );
   }
 }
+
+class DetailedWeatherCard extends StatelessWidget {
+  const DetailedWeatherCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: violetaHover,
+      ),
+      child: Column(
+        children: <Widget>[
+          const Text("Hoje (03/12)", style: mediumSemibold,),
+          const SizedBox(height: 24,),
+          const Icon(Icons.sunny, size: 64,),
+          Text("32°", style: extraLargeBold.copyWith(fontSize: 42),),
+          const Text("Sol entre nuvens"),
+          const SizedBox(height: 24,),
+          Row(
+            children: <Widget>[
+              Container(
+                width: 24,
+                height: 24,
+                margin: const EdgeInsets.only(right: 8),
+                decoration: const BoxDecoration(
+                  color: Color(0xFFFFFFFF),
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  image: DecorationImage(image: AssetImage("assets/img/water_drop.png"), alignment: Alignment.center),
+                ),
+              ),
+              const Text("Umidade:", style: mediumSemibold,),
+              const Expanded(
+                child: Text(
+                  "80%",
+                  textAlign: TextAlign.right,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8,),
+          Row(
+            children: <Widget>[
+              Container(
+                width: 24,
+                height: 24,
+                margin: const EdgeInsets.only(right: 8),
+                decoration: const BoxDecoration(
+                  color: Color(0xFFFFFFFF),
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  image: DecorationImage(image: AssetImage("assets/img/device_thermostat.png"), alignment: Alignment.center),
+                ),
+              ),
+              const Text("Min/Max:", style: mediumSemibold,),
+              const Expanded(
+                child: Text("20/34°", textAlign: TextAlign.right),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
