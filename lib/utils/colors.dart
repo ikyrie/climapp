b458e71ffb6b508f97ad80bc509532f8adf649b7
ikyrie <mah.c5r@gmail.com>
2024-12-03 14:17:08 -0300

    Adicionando uma cor transparente

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color violetaDestaque = Color(0xFF7693FF);
 const Color azulVoid = Color(0xFF01080E);
 const List<Color> gradiente = <Color>[Color(0xFF00457D), Color(0xFF05051F),];
+const Color fillTransparente = Color(0x15FFFFFF);
