3a7f6de7994ac75fbe53e0c7ad36eab7e3ceaf3e
ikyrie <mah.c5r@gmail.com>
2024-12-03 14:17:33 -0300

    Criando o cartão de clima de cidade

---
+++ lib/ui/components/weather_card.dart
@@ -0,0 +1,37 @@
+import 'package:climapp/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class WeatherCard extends StatelessWidget {
+  const WeatherCard({super.key, required this.cityName, required this.temperature, required this.onTap});
+
+  final String cityName;
+  final String temperature;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: InkWell(
+        onTap: () { onTap(); },
+        child: Ink(
+          padding: const EdgeInsets.all(16),
+          decoration: const BoxDecoration(
+            color: fillTransparente,
+            borderRadius: BorderRadius.all(
+              Radius.circular(15),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              const Icon(Icons.sunny),
+              Text(cityName),
+              Text(temperature),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
