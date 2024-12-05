aa3e7621a09a4c1e8427810be9ae8ed3dc0e46ff
ikyrie <mah.c5r@gmail.com>
2024-12-05 11:14:24 -0300

    Escondendo a API Key

--- lib/app/service/weather_service.dart
+++ lib/app/service/weather_service.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 import 'package:climapp/app/models/weather_model.dart';
 import 'package:http/http.dart' as http;
+// flutter run --dart-define=MY_VAR=chave
 
 class WeatherService {
   static const String _baseUrl = 'https://api.hgbrasil.com/weather';
-  static const String _apiKey = '4a661815'; // Substitua pela sua chave da API HG Brasil
+  static const String _apiKey = String.fromEnvironment("API_KEY"); // Substitua pela sua chave da API HG Brasil
 
   Future<WeatherModel> getWeatherByCity(String city) async {
     try {
