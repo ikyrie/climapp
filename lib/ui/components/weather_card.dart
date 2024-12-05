0d309d1cc04254dba9ff7f0faf4f2473c2cdae18
ikyrie <mah.c5r@gmail.com>
2024-12-05 19:12:24 -0300

    Transformando o SingleChildScrollView em um CustomScrollView passando um objeto WeatherModel

--- lib/ui/components/weather_card.dart
+++ lib/ui/components/weather_card.dart
@@ -1,12 +1,12 @@
+import 'package:climapp/app/models/weather_model.dart';
 import 'package:climapp/utils/colors.dart';
 import 'package:climapp/utils/theme.dart';
 import 'package:flutter/material.dart';
 
 class WeatherCard extends StatelessWidget {
-  const WeatherCard({super.key, required this.cityName, required this.temperature, required this.onTap});
+  const WeatherCard({super.key, required this.weather, required this.onTap});
 
-  final String cityName;
-  final String temperature;
+  final WeatherModel weather;
   final Function onTap;
 
   @override
@@ -28,8 +28,8 @@ class WeatherCard extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               const Icon(Icons.sunny),
-              Text(cityName),
-              Text(temperature, style: extraLargeBold,),
+              Text(weather.cityName),
+              Text(weather.temp.toString(), style: extraLargeBold,),
             ],
           ),
         ),
--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -4,6 +4,7 @@ import 'package:climapp/ui/components/weather_card.dart';
 import 'package:climapp/ui/screens/details.dart';
 import 'package:climapp/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 
 class Home extends StatelessWidget {
@@ -24,20 +25,27 @@ class Home extends StatelessWidget {
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter),
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              CustomSearchField(controller: cityTextController),
-              const SizedBox(height: 32,),
-              WeatherCard(cityName: "Florianópolis - SC", temperature: "25°", onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context) => WeatherDetails(cityName: "Florianópolis - SC")));},),
-              WeatherCard(cityName: "Curitiba - PR", temperature: "21°", onTap: (){},),
-              WeatherCard(cityName: "São Paulo - SP", temperature: "32°", onTap: (){},),
-              WeatherCard(cityName: "Porto Alegre - RS", temperature: "19°", onTap: (){},),
-              WeatherCard(cityName: "Toledo - PR", temperature: "26°", onTap: (){},),
-            ],
-          ),
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverToBoxAdapter(
+              child: CustomSearchField(controller: cityTextController),
+            ),
+            const SliverToBoxAdapter(child: SizedBox(height: 32,),),
+          Observer(
+            builder: (context) {
+              return SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    final weather = weatherStore.weatherList[index];
+                    return WeatherCard(weather: weather, onTap: (){},);
+                  },
+                  childCount: weatherStore.weatherList.length,
+                ),
+              );
+            },
+          ),          ],
         ),
-      ),
+        ),
     );
   }
 }
