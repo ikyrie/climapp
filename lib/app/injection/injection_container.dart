eb9b6f3113958f9010f8ece3600df127897fa11d
ikyrie <mah.c5r@gmail.com>
2024-12-05 17:29:51 -0300

    Adicionando a dependência do WeatherStore no nosso injection_container

--- lib/app/injection/injection_container.dart
+++ lib/app/injection/injection_container.dart
@@ -1,5 +1,6 @@
 import 'package:climapp/app/adapters/http_adapter.dart';
 import 'package:climapp/app/service/weather_service.dart';
+import 'package:climapp/app/store/weather_store.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,4 +9,5 @@ final GetIt locator = GetIt.instance;
 void setupLocator() {
   locator.registerLazySingleton<IHttpClient>(() => HttpAdapter(http.Client()));
   locator.registerLazySingleton(() => WeatherService(locator.get<IHttpClient>()));
+  locator.registerLazySingleton(() => WeatherStore());
 }
